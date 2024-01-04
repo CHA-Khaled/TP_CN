e4c0be8b1ea64d5738ae24e3c492b79cef4614d8
Khaled <khaledchabane59@gmail.com>
2024-01-04 18:44:46 +0100

    Adding the defintion eig_poisson1D to lib_poisson1D.h

--- include/lib_poisson1D.h
+++ include/lib_poisson1D.h
@@ -22,3 +22,4 @@ void write_xy(double* vec, double* x, int* la, char* filename);
 int indexABCol(int i, int j, int *lab);
 int dgbtrftridiag(int *la, int *n, int *kl, int *ku, double *AB, int *lab, int *ipiv, int *info);
 double relative_forward_error(double* x, double* y, int* la);
+void eig_poisson1D(double* eigval, int *la);
