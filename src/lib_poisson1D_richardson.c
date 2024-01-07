b3d26cfd92fa5b0aef830fa43e68784f615ceed7
Khaled <khaledchabane59@gmail.com>
2024-01-07 20:08:56 +0100

    ADDING eigmax_poisson1D function

--- src/lib_poisson1D_richardson.c
+++ src/lib_poisson1D_richardson.c
@@ -36,9 +36,45 @@ void eig_poisson1D(double* eigval, int *la){
 }
 
 
-double eigmax_poisson1D(int *la){
-  return 0;
+double eigmax_poisson1D(int *la) {
+    int n = *la;
+    int lda = n;
+    double *AB = (double *)malloc(n * n * sizeof(double));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==j) {
+                AB[i * n + j] = 2.0;
+            }else if(abs(i - j) == 1){
+                AB[i * n + j] = -1.0;
+            }else{
+                AB[i * n + j] = 0.0;
+        }
+     }
+    }
+
+    int info;
+    double work_query;
+    int lwork = -1;
+    LAPACK_dsyev("V", "U", &n, AB, &lda, NULL, &work_query, &lwork, &info);
+
+    lwork = (int)work_query;
+    double *work = (double *)malloc(lwork * sizeof(double));
+    double *eigval = (double *)malloc(n * sizeof(double));
+
+    LAPACK_dsyev("V", "U", &n, AB, &lda, eigval, work, &lwork, &info);
+    if (info != 0) {
+        fprintf(stderr, "Erreur dans la fonction LAPACK dsyev (info = %d)\n", info);
+    }
+
+    double eigmax = eigval[n - 1];
+
+    free(AB);
+    free(work);
+    free(eigval);
+
+    return eigmax;
 }
+
 
 double eigmin_poisson1D(int *la){
   return 0;
