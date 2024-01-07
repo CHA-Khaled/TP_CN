6f024293ef64802f81eb83d0c9c1ceac2134aa1f
Khaled <khaledchabane59@gmail.com>
2024-01-07 19:59:57 +0100

    completing the eig_poisson1D function

--- src/lib_poisson1D_richardson.c
+++ src/lib_poisson1D_richardson.c
@@ -6,7 +6,35 @@
 #include "lib_poisson1D.h"
 
 void eig_poisson1D(double* eigval, int *la){
+    int n= *la;
+    int lda=n;
+    double *AB = (double *)malloc(n * n * sizeof(double));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==j) {
+                AB[i * n + j] = 2.0;
+            }else if(abs(i - j) == 1){
+                AB[i * n + j] = -1.0;
+            }else{
+                AB[i * n + j] = 0.0;
+        }
+     }
+    }
+    int info;
+    double work_query;
+    int lwork = -1;
+    LAPACK_dsyev("V", "U", &n, AB, &lda, eigval, &work_query, &lwork, &info);
+    lwork = (int)work_query;
+    double *work = (double *)malloc(lwork * sizeof(double));
+    LAPACK_dsyev("V", "U", &n, AB, &lda, eigval, work, &lwork, &info);
+    if(info != 0){
+        fprintf(stderr, "Erreur dans la fonction LAPACK dsyev (info = %d)\n", info);
+    }
+
+    free(AB);
+    free(work);
 }
+
 
 double eigmax_poisson1D(int *la){
   return 0;
