952488248750a10c0f89669feec5ce7cc97b221b
Khaled <khaledchabane59@gmail.com>
2024-01-08 21:34:08 +0100

    Adding the header of dcsrmv

--- include/lib_poisson1D.h
+++ include/lib_poisson1D.h
@@ -31,3 +31,4 @@ void richardson_alpha(double *AB, double *RHS, double *X, double *alpha_rich, in
 void extract_MB_jacobi_tridiag(double *AB, double *MB, int *lab, int *la,int *ku, int*kl, int *kv);
 void extract_MB_gauss_seidel_tridiag(double *AB, double *MB, int *lab, int *la,int *ku, int*kl, int *kv);
 void richardson_MB(double *AB, double *RHS, double *X, double *MB, int *lab, int *la,int *ku, int*kl, double *tol, int *maxit, double *resvec, int *nbite);
+void dcsrmv(int m, int n, int nnz, double *values, int *col_indices, int *row_ptr, double *x, double *y);
